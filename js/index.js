c65073ce229f41ec6262f837c87111f52b43d0f0
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-03-02 21:33:02 +0200

    declaring constants and variables

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+let playerTurn;
+let computerTurn;
+let order =[] //order of flashing
+let playerOrder=[] //player entries
+
+const play=document.getElementById("play")
+const level =document.getElementById("level")
+const green=document.getElementsByClassName("tile green inactive")
+const red=document.getElementsByClassName("tile red inactive")
+const blue=document.getElementsByClassName("tile blue inactive")
+const yellow =document.getElementsByClassName("tile yellow inactive")
+
+
+player.addEventListener("click",(event)=>{
+
+})
